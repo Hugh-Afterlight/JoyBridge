ee0e130779021feb83ba7949dd00811387cb9d61
hugh <ht3515@gmail.com>
2026-05-07 19:50:02 +0800

    Initial Commit

---
+++ JoyBridge/JoyBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoyBridgeApp.swift
+//  JoyBridge
+//
+//  Created by Hugh on 5/7/26.
+//
+
+import SwiftUI
+
+@main
+struct JoyBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
